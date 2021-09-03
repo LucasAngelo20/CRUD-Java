d8b09d276d7032fa1fc8d1f6b30a4fcedc630795
LucasAngelo20 <lucasga84@gmail.com>
2021-09-03 15:35:27 -0300

    Aula de metodos feita

---
+++ src/main/java/Aula/de/java/AulaDeJava/controllers/ClienteController.java
@@ -0,0 +1,29 @@
+package Aula.de.java.AulaDeJava.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import Aula.de.java.AulaDeJava.models.Cliente;
+
+@RestController
+@RequestMapping(path = "/clientes")
+public class ClienteController {
+	
+	@GetMapping(path = "/qualquer")
+	public Cliente obterCliente() {
+		return new Cliente(20, "Lucas", "123.456.789-00");
+	}
+	
+	@GetMapping("/{id}")
+	public Cliente obterClientePorId1(@PathVariable int id) {
+		return new Cliente(id, "Isabelle", "987.654.321-00");
+	}
+	@GetMapping
+	public Cliente obterClientePorId2(
+			@RequestParam(name = "id", defaultValue = "1") int id) {
+		return new Cliente(id, "Milena", "111.222.333-00");
+	}
+}
--- src/main/java/Aula/de/java/AulaDeJava/controllers/PrimeiroController.java
+++ src/main/java/Aula/de/java/AulaDeJava/controllers/PrimeiroController.java
@@ -1,14 +1,15 @@
 package Aula.de.java.AulaDeJava.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PrimeiroController {
 	
-	@RequestMapping(method = RequestMethod.GET)
+	@GetMapping(path = {"/ola" , "/saudacao"})
 	public String ola() {
 		return "Olá mundo!";
 	}
+	
 }
